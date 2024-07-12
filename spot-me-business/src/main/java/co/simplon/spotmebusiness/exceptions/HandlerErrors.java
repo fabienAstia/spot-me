f6bd41fcd1fcabfcbfd6ebf0b0920859bd9194ea
Fabien Astiasaran <fabien.astiasaran@gmail.com>
2024-07-12 21:49:45 +0200

    SCMCON-0000 refactoring the HandlerExceptions -> add call in static class to FieldsExceptions

--- spot-me-business/src/main/java/co/simplon/spotmebusiness/exceptions/HandlerErrors.java
+++ spot-me-business/src/main/java/co/simplon/spotmebusiness/exceptions/HandlerErrors.java
@@ -14,15 +14,31 @@ public class HandlerErrors extends RuntimeException {
 		this.fieldErrorsList = fieldErrorsList;
 	}
 
+	public HandlerErrors() {
+	}
+
+	public List<FieldErrors> getFieldErrorsList() {
+		return fieldErrorsList;
+	}
+
+	public void setFieldErrorsList(List<FieldErrors> fieldErrorsList) {
+		this.fieldErrorsList = fieldErrorsList;
+	}
+
+	public List<GlobalErrors> getGlobalErrorsList() {
+		return globalErrorsList;
+	}
+
+	public void setGlobalErrorsList(List<GlobalErrors> globalErrorsList) {
+		this.globalErrorsList = globalErrorsList;
+	}
+
 	public static class FieldErrors {
 
-		private final static Exception [] fieldExceptions = {new FileSizeException(),  new FileTypeException()};
+		private final static Exception FILESIZE = new FileSizeException();
+		private final static Exception FILETYPE = new FileTypeException();
 
-
-		public FieldErrors(String fieldException){
-//
-//			stream.of(fieldExceptions).anyMatch
+		public FieldErrors() {
 		}
-
 	}
 }
